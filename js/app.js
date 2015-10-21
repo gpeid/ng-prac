2ae0e1eeceb3976967bdaf4511dd499430c602ee
George <george.diep@curiositystream.com>
2015-10-21 13:25:26 -0400

    sharing data 3

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 angular.module('app', ["ui.router"])
 // a state for controller
 	.config(function config($stateProvider) {
-	$stateProvider.state("index", {
-		url:"";
+	$stateProvider.state("ng-sharing-data.html", {
+		url: "",
 		controller: "FirstCtrl as first",
 		templateUrl: "templates/first.html"
 	})
